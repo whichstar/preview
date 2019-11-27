34c6aa8b417ddc6732448d203c6e8f68bf71a659
whichstar <821296576@qq.com>
2019-11-27 17:10:36 +0800

    first commit

---
+++ src/test/java/com/wqx/view/preview/PreviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wqx.view.preview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
